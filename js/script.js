96906d91faccb0fa370852cf0293d568d1ec97e0
FJSTAR <jigarfulia31@gmail.com>
2024-10-25 21:59:17 +0530

    Added filter for portfolio projects

--- js/script.js
+++ js/script.js
@@ -33,3 +33,36 @@ function asideSectionTogglerBtn() {
         allSection[i].classList.toggle("open");
     }
 }
+
+// Portfolio filter tabs
+(function($) {
+
+    'use strict';
+  
+    var $filters = $('.filter [data-filter]'),
+      $boxes = $('.boxes [data-portfolio]');
+  
+    $filters.on('click', function(e) {
+      e.preventDefault();
+      var $this = $(this);
+      
+      $filters.removeClass('active');
+      $this.addClass('active');
+  
+      var $filterColor = $this.attr('data-filter');
+  
+      if ($filterColor == 'all') {
+        $boxes.removeClass('is-animated')
+          .fadeOut().promise().done(function() {
+            $boxes.addClass('is-animated').fadeIn();
+          });
+      } else {
+        $boxes.removeClass('is-animated')
+          .fadeOut().promise().done(function() {
+            $boxes.filter('[data-portfolio = "' + $filterColor + '"]')
+              .addClass('is-animated').fadeIn();
+          });
+      }
+    });
+  
+  })(jQuery);
